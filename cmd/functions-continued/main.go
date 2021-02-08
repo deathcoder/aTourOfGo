16fbf651057da26a8635a6eb0190e50f419297af
dcapozzi <davide.capozzi@objectway.it>
2021-02-08 14:37:55 +0100

    Functions continued

---
+++ cmd/functions-continued/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(x, y int) int {
+	return x + y
+}
+
+// When two or more consecutive named function parameters share a type, you can omit the type from all but the last.
+// In this example, we shortened
+//   x int, y int
+// to
+//   x, y int
+func main() {
+	fmt.Println(add(42, 13))
+}
